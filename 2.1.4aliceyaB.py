08aa7be367dbe2404a04455b0240373153eb6b09
Aliceya3123 <55498147+Aliceya3123@users.noreply.github.com>
2020-02-19 10:16:09 -0500

    Add files via upload

--- 2.1.4aliceyaB.py
+++ 2.1.4aliceyaB.py
@@ -7,20 +7,15 @@ import tkinter as tk
 # main window
 root = tk.Tk()
 root.wm_geometry("200x200")
-
 root.title("Authorization")
+
 frame_login = tk.Frame(root)
 frame_login.grid(row=0, column=0, sticky="news" )
-
-
-
-frame_login = tk.Frame(root)
-frame_login.grid()
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack(padx=50) 
 
-ent_username = tk.Entry(frame_login, bd=3, show = "*") 
+ent_username = tk.Entry(frame_login, bd=3) 
 ent_username.pack()
 
 lbl_password = tk.Label(frame_login, text='password:')
@@ -30,23 +25,21 @@ ent_password = tk.Entry(frame_login, bd=3, show = "*")
 ent_password.pack()
 
 def test_button():
-    print("click")
+    print("working")
     frame_auth.tkraise()
+
+    password = ent_password.get()
+    lbl_display_pass.config(text = password)
 
 
 button_login = tk.Button(frame_login, text='Login:', command = test_button)
 button_login.pack()
 
-#Authorization
 frame_auth = tk.Frame(root)
 frame_auth.grid(row=0, column=0, sticky="news" )
 
-
-
-
-
-
-
+lbl_display_pass= tk.Label(frame_auth, text = "test")
+lbl_display_pass.pack()
 
 
 frame_login.tkraise()
